3277379f2e80a33b1e2f36fa932af445f9af78ef
gel00 <gellaigabor@gmail.com>
2021-04-22 22:37:37 +0100

    deleted comments

--- script.js
+++ script.js
@@ -7,30 +7,7 @@ for (i = 0; i <= 1; i += 0.07) {
   times.unshift(450 - Math.ceil(50 + 3.5 * x * 100));
 }
 
-console.log(times);
-const animate = (el, callback = () => {}) => {
-  el.animate(
-    [
-      {
-        transform: "rotate(0deg)",
-      },
-      {
-        transform: "rotate(-720deg)",
-      },
-    ],
-    {
-      duration: 2000,
-      fill: "forwards",
-      iterations: 1,
-    }
-  ).finished.then(() => callback());
-};
-
-//animate(ball1, () => animate(ball2));
-
-const anims = [];
-
-const move = async (el, y0, y1, duration = 700) => {
+const moveY = async (el, y0, y1, duration = 700) => {
   const distance = Math.abs(y0 - y1);
   duration = (distance / 350) * duration;
   const aOBJ = el.animate(
@@ -49,7 +26,6 @@ const move = async (el, y0, y1, duration = 700) => {
       easing: y0 < y1 ? "ease-in" : "ease-out",
     }
   );
-  anims.push(aOBJ);
   return aOBJ.finished;
 };
 const moveX = async (el, x0, x1, duration = 700) => {
@@ -70,19 +46,8 @@ const moveX = async (el, x0, x1, duration = 700) => {
       iterations: 1,
     }
   );
-  anims.push(aOBJ);
   return aOBJ.finished;
 };
-
-/*bounce(ball1, ball1.offsetTop, 400).then(() => {
-  bounce(ball1, 400, 200).then(() => {
-    bounce(ball1, 200, 400).then(() => {
-      bounce(ball1, 400, 300).then(() => {
-        bounce(ball1, 300, 400);
-      });
-    });
-  });
-});*/
 
 async function asyncForEach(array, callback) {
   for (let index = 0; index < array.length; index++) {
@@ -98,11 +63,9 @@ async function asyncLoop(callback) {
 const bounce = async (el) => {
   await asyncForEach(times, async (time, index, times) => {
     let yMax = times[times.length - 1];
-    await move(el, time, yMax);
-    await move(el, yMax, times[index + 1] || yMax);
+    await moveY(el, time, yMax);
+    await moveY(el, yMax, times[index + 1] || yMax);
   });
-  //let ani = await move(ball1, ball1.offsetTop, 400);
-  //ani = await move(ball1, 400, 200);
 };
 
 const infinityRoll = async (el, speed) => {
@@ -143,49 +106,17 @@ const roll = async (
   animation.deg = deg;
   return animation.finished;
 };
-const myAnimation = async () => {
-  infinityRoll(ball1, 1000);
-  await moveX(ball1, ball1.offsetLeft, ball1.offsetLeft - 35, 10000);
-  bounce(ball1).then(() => {
-    const animations = ball1.getAnimations();
-    setTimeout(() => {
-      animations[0].pause();
-      animations[1].pause();
-      animations[2].pause();
-      animations[3].pause();
-      animations[4].pause();
-      animations[5].pause();
-      animations[6].pause();
-      animations[7].pause();
-    }, 500);
-  });
-  moveX(ball1, ball1.offsetLeft, ball1.offsetLeft - 5000, 4000);
-};
 
 const myAnimation2 = async () => {
   //roll ball to the edge
-  await roll(ball1, 35, 2000, "left", 0);
-  await (() => {
-    roll(ball1, 300, 3500, "left", -114.65);
-    //moveX(ball1, ball1.offsetLeft, ball1.offsetLeft - 35, 10000);
-    bounce(ball1);
-  })();
+  bounce(ball2);
+  setTimeout(async () => {
+    await roll(ball1, 35, 2000, "left", 0);
+    await (() => {
+      roll(ball1, 300, 3500, "left", -114.65);
+      bounce(ball1);
+    })();
+  }, 650);
 };
 
 myAnimation2();
-/*const waitFor = (ms) => new Promise((r) => setTimeout(r, ms));
-
-async function asyncForEach(array, callback) {
-  for (let index = 0; index < array.length; index++) {
-    await callback(array[index], index, array);
-  }
-}
-
-const start = async () => {
-  await asyncForEach([1, 2, 3], async (num) => {
-    await waitFor(500);
-    console.log(num);
-  });
-  console.log("Done");
-};
-start();*/
