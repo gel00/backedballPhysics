f8b44ea489b177ab3bad755a3c45787ba390855e
gel00 <gellaigabor@gmail.com>
2021-04-23 13:16:04 +0100

    added comments

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 const ball1 = document.getElementById("ball1");
 const ball2 = document.getElementById("ball2");
 
-const times = [];
+// This fn returns an array of distances for the bounce animation
+//elasticity has to be between: 5 , 0.1
+const getParabola = (totalYDistance, ballsize, elasticity) => {
+  const yCoordinates = [];
+  for (i = 0; i <= 1; i += elasticity / 10) {
+    let x = Math.pow(i, 5);
+    yCoordinates.unshift(
+      totalYDistance + ballsize - Math.ceil(ballsize + 3.5 * x * 100)
+    );
+  }
+  return yCoordinates;
+};
+//an array Y cordinates, that mimics the physics of a basketball
+const bounceDistances = getParabola(400, 50, 0.75);
 
-for (i = 0; i <= 1; i += 0.07) {
-  let x = Math.pow(i, 5);
-  times.unshift(450 - Math.ceil(50 + 3.5 * x * 100));
-}
-
+//transform Y animation, from y0 to y1
+//returns a promise
 const moveY = async (el, y0, y1, duration = 700) => {
   const distance = Math.abs(y0 - y1);
   duration = (distance / 350) * duration;
@@ -29,6 +39,9 @@ const moveY = async (el, y0, y1, duration = 700) => {
   );
   return aOBJ.finished;
 };
+
+//transform x animation, from x0 to x1
+//returns a promise
 const moveX = async (el, x0, x1, duration = 700) => {
   const distance = Math.abs(x0 - x1);
   duration = (distance / 350) * duration;
@@ -49,32 +62,24 @@ const moveX = async (el, x0, x1, duration = 700) => {
   );
   return aOBJ.finished;
 };
-
+// an async foreach
 async function asyncForEach(array, callback) {
   for (let index = 0; index < array.length; index++) {
     await callback(array[index], index, array);
   }
 }
-async function asyncLoop(callback) {
-  for (let index = 0; index < 1000; index++) {
-    await callback();
-  }
-}
 
+// an async foreach over the parabolicYCordinates
+// each iteration execute a tansitionY animation
 const bounce = async (el) => {
-  await asyncForEach(times, async (time, index, times) => {
-    let yMax = times[times.length - 1];
+  await asyncForEach(bounceDistances, async (time, index, bounceDistances) => {
+    let yMax = bounceDistances[bounceDistances.length - 1];
     await moveY(el, time, yMax);
-    await moveY(el, yMax, times[index + 1] || yMax);
+    await moveY(el, yMax, bounceDistances[index + 1] || yMax);
   });
 };
 
-const infinityRoll = async (el, speed) => {
-  await asyncLoop(async () => {
-    await roll(el, speed);
-  });
-};
-
+// Calculate the angle of rotation based on distance and speed by using the  circumference of the element.
 const roll = async (
   el,
   distance,
@@ -108,12 +113,17 @@ const roll = async (
   return animation.finished;
 };
 
+//animation to run
 const myAnimation2 = async () => {
-  //roll ball to the edge
+  //drop ball2
   bounce(ball2);
+  //after 650ms
   setTimeout(async () => {
+    //roll ball1 to the edge
     await roll(ball1, 35, 2000, "left", 0);
+    //than drop ball1 and roll further
     await (() => {
+      //hardcoded start angle and duration
       roll(ball1, 300, 3500, "left", -114.65);
       bounce(ball1);
     })();
